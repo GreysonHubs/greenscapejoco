4fc636f5570650dddea5f92a3b10df93e63f6c5e
NaturesNectorStand <hublergreyson@gmail.com>
2025-05-18 17:39:01 -0500

    update v2.1.0-booking platform

--- javascript/script.js
+++ javascript/script.js
@@ -1,11 +1,13 @@
 document.getElementById('snowalertcloseicon').addEventListener('click', function() {
     var snowAlert = document.getElementById('snowalert');
+    var navbar = document.getElementById('navbarcasing');
     snowAlert.style.transition = 'max-height 0.5s ease-out, padding 0.5s ease-out';
     snowAlert.style.overflow = 'hidden';
     snowAlert.style.maxHeight = '0';
     snowAlert.style.padding = '0';
     setTimeout(function() {
         snowAlert.style.display = 'none';
+        navbar.style.display = 'none';
     }, 500);
 });
 
